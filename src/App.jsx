d53bd8df8cfb2375110ca5bf0113b4dda8f22b60
phan-phuong-an <phanthiphuongan1412@gmail.com>
2026-06-30 14:06:20 +0700

    feat: init project with vite, react and tailwind v4

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+import React from 'react';
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+      <h1 className="text-4xl font-bold tracking-wider text-cyan-400 animate-pulse">
+        Kính Thực Tế Ảo VR - HELICORP Project 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
